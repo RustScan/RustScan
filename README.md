d4d8c77906fc40cac7e294ef50184ca87a0e1273
Brandon <brandonskerritt51@gmail.com>
2020-07-12 01:40:57 +0100

    Goodnight!

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # RustScan
 An port scanner in Rust.
 
-Once it finds open ports, it runs by default nmap -Sc -p $ports. This command can be changed by adding a settings.yml file to APPDIRS (documentation here).
+1. Find ports quickly in Rust.
+2. Automatically runs `nmap -A -sV -p $ports -vvv` or whatever command is in settings.yml
+3. Profit???
 
+# Ideas
+
+- [ ] Do popular ports first (80, 445, 8080, 21, 22)
+- [ ] Option for top 1k ports
+- [ ] Affter 1k ports print the nmap command to scan the first 1k so user can run while rustscanner finishes
+- [ ] Tar pit prevention (look for different sized payloads)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::net;
 use colored::*;
-use clap::Arg;
-
+use clap::{App, crate_authors};
 
 fn main() {
+    let _matches = App::new("RustScan")
+        .author(crate_authors!())
+        .about("Fast Port Scanner built in Rust")
+        .version("0.01")
+    
+        .get_matches();
+    print_opening()
+    }
+fn print_opening(){
     let s = "
     _____           _    _____                                 
     |  __ \\         | |  / ____|                                
@@ -13,4 +20,4 @@ fn main() {
     |_|  \\_\\__,_|___/\\__|_____/ \\___\\__,_|_| |_|_| |_|\\___|_|  "; 
         println!("{} \n {} \n {}", s.green(), "Automated Decryption Tool - https://github.com/ciphey/ciphey".red(),"Creator https://github.com/brandonskerritt".green());
     
-    }
+}
