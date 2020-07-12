3e619594b2dc15df61f61d5d41eb0a9ccee68f24
Brandon <brandonskerritt51@gmail.com>
2020-07-12 18:33:52 +0100

    Adding broken Rust code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use colored::*;
 use clap::{App, crate_authors, Arg};
+use std::net::{IpAddr, TcpStream};
+use std::{str::FromStr, io};
+// Upper Port Limit
+const MAX: u16 = 65535;
+
+
 
 fn main() {
-    let _matches = App::new("RustScan")
+    let matches = App::new("RustScan")
         .author(crate_authors!())
         .about("Fast Port Scanner built in Rust")
         .version("0.01")
@@ -14,7 +20,12 @@ fn main() {
         .arg(Arg::with_name("p")
             .help("The port range you want to scan"))
         .get_matches();
-    print_opening()
+    print_opening();
+
+    // validatses the IP address and turns it into an IpAddr type
+    let addr = IpAddr::from_str(&matches.free[0])
+        .expect("IPADDR must be a valid IPv4 or IPv6 address");
+    
     
     // let _nmap: &str = "nmap -A -sV -vvv -p $ports $ipaddr"
 }
@@ -28,4 +39,19 @@ fn print_opening(){
     |_|  \\_\\__,_|___/\\__|_____/ \\___\\__,_|_| |_|"; 
         println!("{} \n {} \n {}", s.green(), "Automated Decryption Tool - https://github.com/ciphey/ciphey".red(),"Creator https://github.com/brandonskerritt".green());
     
+}
+
+fn scan(ip: IpAddr, start_port: u16){
+    // ports is a list slice of X ports
+    // This depends on the threads useud
+    // Usually around ~5 ports
+    // 
+    match TcpStream::connect((ip, start_port)) {
+        Ok(_) => {
+            // Found open port, indicate progress and send to main thread
+            print!("Found ip {} and port {}", ip, start_port);
+            io::stdout().flush().unwrap();
+        }
+        Err(_) => {}
+
 }
