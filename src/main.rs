c79c35c0b324fdbf231b52672f1c61c321f22b42
Brandon <brandonskerritt51@gmail.com>
2020-07-13 21:47:40 +0100

    More work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use colored::*;
 use clap::{App, crate_authors, Arg};
-use std::net::{IpAddr, TcpStream};
-use std::{str::FromStr, io};
-// Upper Port Limit
-const MAX: u16 = 65535;
+use std::{str::FromStr, net::{IpAddr, TcpStream}, ops::Range, u16};
+use rayon::prelude::*;
+use arrayvec::ArrayVec;
 
+// Upper Port Limit
+const NUM: u32 = 65535;
 // The default config for users
 struct Config {
     ip: IpAddr,
@@ -24,28 +25,74 @@ fn main() {
         .arg(Arg::with_name("i")
             .required(true)
             .index(1)
+            .long("--ip-address")
             .help("The IP address to scan"))
         .arg(Arg::with_name("p")
-        .   index(2)
+            .index(2)
+            .long("--ports")
             .help("The port range you want to scan"))
         .get_matches();
 
     print_opening();
 
-    if matches.is_present("i"){
-        println!("IP address was used!")
+    let ip = matches.value_of("i").unwrap_or("None");
+
+    // get the value and store in a1_val
+    //if let Some(ip) = matches.value_of("i"){
+     //   println!("{}", ip);
+    //}
+
+    if ip == "None"{
+        println!("{}", "Error: No input was given.".red());
+        return ();
     }
 
     // validatses the IP address and turns it into an IpAddr type
-    //let addr = IpAddr::from_str(&matches.free[0])
-    //    .expect("IPADDR must be a valid IPv4 or IPv6 address");
+    let addr = IpAddr::from_str(&ip)
+        .expect("IPADDR must be a valid IPv4 or IPv6 address");
+
+    println!("IP is {}", addr);
 
     // if ports not specified, use this:
-    let ports = 0..MAX;
+
+    // creates array of ports up to max port num
+    /*let mut ports: [i32; 65535] = [0; NUM];
+    for (i, v) in ports.iter_mut().enumerate() {
+        *v = i as i32
+    }*/
+    let ports = 0..100;
+    perform_scan(addr, ports);
+    // println!("{}. {}", addr, port);
+    // scan(addr, port)
+
+
+    //scan(addr, ports)
     
     
     // let _nmap: &str = "nmap -A -sV -vvv -p $ports $ipaddr"
 }
+
+/// Performs the scan
+fn perform_scan(addr: IpAddr, ports: [u16]){
+    // TODO would be best if IpAddr was global
+    let r = (0..NUM).into_par_iter()
+                   .map(|port: u16| scan(addr, port));
+    ports.par_iter().map(|port| scan(addr, port)
+
+}
+
+fn scan(addr: IpAddr, port:u16) {
+    println!("Running scan");
+    match TcpStream::connect((addr, port)) {
+        Ok(_) => {
+            // Found open port, indicate progress and send to main thread
+            print!("Found ip {} and port {}", addr, port);
+        }
+        Err(_) => {}
+    }
+
+}
+
 fn print_opening(){
     let s = "
      _____           _    _____                 
@@ -57,19 +104,4 @@ fn print_opening(){
     Faster nmap scanning with rust."; 
         println!("{} \n {} \n {}", s.green(), "Automated Decryption Tool - https://github.com/ciphey/ciphey".red(),"Creator https://github.com/brandonskerritt".green());
     
-}
-
-fn scan(ip: IpAddr, start_port: u16){
-    // ports is a list slice of X ports
-    // This depends on the threads useud
-    // Usually around ~5 ports
-    // 
-    match TcpStream::connect((ip, start_port)) {
-        Ok(_) => {
-            // Found open port, indicate progress and send to main thread
-            print!("Found ip {} and port {}", ip, start_port);
-            io::stdout().flush().unwrap();
-        }
-        Err(_) => {}
-
 }
