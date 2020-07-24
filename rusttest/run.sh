b609f40bd246a0cbae502bc80c427a79e2ee9de8
Brandon <brandonskerritt51@gmail.com>
2020-07-24 14:26:36 +0100

    rust builder added

--- rusttest/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build -t rustscan-builder . || exit
-
-# This creates a 'volume' which binds your currentdirectory/debs to 
-# the location where the deb files get spat out in the container.
-# You don't need to worry about it. Just chmod +x run.sh && ./run.sh and
-# you'll get yer .deb file in a few minutes.
-docker run -v "$(pwd)/debs:/RustScan/target/debian" rustscan-builder
