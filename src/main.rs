d8fd12694c4281d63166a522e5ca1a993bf80719
Brandon <brandonskerritt51@gmail.com>
2020-07-31 11:32:02 +0100

    fixed tests

--- src/main.rs
+++ src/main.rs
@@ -228,7 +228,7 @@ mod tests {
             Err(_) => {panic!("Could not parse IP Address")}
         };
         let scanner = Scanner::new(addr, 1, 
-        65535, 1000, Duration::from_millis(10), true, false);
+        65535, 1000, Duration::from_millis(10), true);
         let scan_result = block_on(scanner.run());
         // if the scan fails, it wouldn't be able to assert_eq! as it panicked!
         assert_eq!(1, 1);
@@ -239,7 +239,7 @@ mod tests {
             Ok(res) => {res}
             Err(_) => {panic!("Could not parse IP Address")}
         };
-        let scanner = Scanner::new(addr, 1, 65535, 1000, Duration::from_millis(10), true, true);
+        let scanner = Scanner::new(addr, 1, 65535, 1000, Duration::from_millis(10), true);
         let scan_result = block_on(scanner.run());
         // if the scan fails, it wouldn't be able to assert_eq! as it panicked!
         assert_eq!(1, 1);
