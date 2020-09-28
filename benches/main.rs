81ad00dc29fe78842a550e99bd5c9087dffa06d8
Brandon <brandonskerritt51@gmail.com>
2020-09-29 00:29:58 +0100

    1.10.0 release

---
+++ benches/main.rs
@@ -0,0 +1,9 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rustscan::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
