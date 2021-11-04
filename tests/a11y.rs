1678fd4e883ce1e68426ca35e760b5f4157785b9
fack <>
2021-11-03 21:47:03 -0500

    something wrong w/ test still

--- tests/a11y.rs
+++ tests/a11y.rs
@@ -8,36 +8,41 @@
  */
 
 use regex::Regex;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn rustscan_a11y_test(args: &[&str]) {
     println!("Running: target/debug/rustscan: {}", args.join(" "));
 
-    let mut output = Command::new("target/debug/rustscan").args(args).output()?;
+    let output = Command::new("target/debug/rustscan")
+        .args(args)
+        .stdout(Stdio::piped())
+        .output()
+        .unwrap();
 
-    if !output.status.success() {
-        error_chain::bail!("Command exec failed");
-    }
-
-    let pattern = Regex::new(r"( ?[!~>|{}] ?)",)?;
-
-    if pattern.is_match(output.stdout) {
-        panic!("stdout contains non-accessible characters");
-    }
+    let pattern = Regex::new(r"( ?[!~>|{}] ?)").unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    println!("{}", stdout);
+    assert!(pattern.is_match(&stdout));    
+    // if !pattern.is_match(&stdout) {
+    //     println!("pass");
+    // }
+    // else {panic!("shits broke yo");}
 }
+
+
 
 mod accesstests {
 
     #[test]
     #[ignore]
     fn scan_localhost() {
-        super::run_rustscan_a11y_test(&["--accessible", "--greppable", "--no-nmap", "127.0.0.1"]);
+        super::rustscan_a11y_test(&["--accessible", "--greppable", "--no-nmap", "127.0.0.1"]);
     }
 
     #[test]
     #[ignore]
     fn scan_google_com() {
-        super::run_rustscan_a11y_test(&[
+        super::rustscan_a11y_test(&[
             "--accessible",
             "--greppable",
             "--no-nmap",
@@ -48,4 +53,38 @@ mod accesstests {
             "google.com",
         ]);
     }
+
+    #[test]
+    fn scan_example_com() {
+        super::rustscan_a11y_test(
+            &[
+                "--accessible",
+                "--greppable",
+                "--no-nmap",
+                "-u",
+                "5000",
+                "-b",
+                "2500",
+                "example.com",
+            ],
+        );
+    }
+
+    #[test]
+    #[ignore]    
+    fn scan_rustscan_cmnatic_co_uk() {
+        super::rustscan_a11y_test(
+            &[
+                "--accessible",
+                "--greppable",
+                "--no-nmap",
+                "-u",
+                "5000",
+                "-b",
+                "2500",
+                "rustscan.cmnatic.co.uk/~tilde",
+            ],
+        );
+    }
+
 }
