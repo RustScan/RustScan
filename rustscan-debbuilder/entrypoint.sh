ef613f3f71affc1dc8a1670439b525fd1515d9c3
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2022-05-24 14:59:02 -0700

    add raspberry pi support

--- rustscan-debbuilder/entrypoint.sh
+++ rustscan-debbuilder/entrypoint.sh
@@ -10,6 +10,10 @@ cargo deb
 rustup target add arm-unknown-linux-gnueabihf
 cargo deb --target=arm-unknown-linux-gnueabihf
 
+#armv7
+rustup target add armv7-unknown-linux-gnueabihf
+cargo deb --target=armv7-unknown-linux-gnueabihf
+
 #i386
 rustup target add i686-unknown-linux-gnu
 cargo deb --target=i686-unknown-linux-gnu
