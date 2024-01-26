cb4b3b334dc2d8c7001cefbe37b75a5c97dc9611
Wuelfhis Asuaje <wasuaje@shorecg.com>
2024-01-26 11:48:50 -0300

    disabling a not desired println! used for debugging purpose

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,7 @@ fn main() {
         opts.accessible,
         opts.exclude_ports.unwrap_or_default()
     );
-    println!("Scanner finished building: {:?}", scanner);
+    dbg!("Scanner finished building: {:?}", scanner);
 
     let mut portscan_bench = NamedTimer::start("Portscan");
     let scan_result = block_on(scanner.run());
