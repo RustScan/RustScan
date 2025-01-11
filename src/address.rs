1d07aaeeb04ba9ad486a886203e9f97592c71f4e
Vrtgs <mirza123.the.best@gmail.com>
2025-01-11 17:22:10 +0300

    fix tests

--- src/address.rs
+++ src/address.rs
@@ -208,14 +208,14 @@ mod tests {
     use super::{get_resolver, parse_addresses, Opts};
     use std::net::Ipv4Addr;
 
-    #[test]
-    fn parse_correct_addresses() {
+    #[tokio::test]
+    async fn parse_correct_addresses() {
         let opts = Opts {
             addresses: vec!["127.0.0.1".to_owned(), "192.168.0.0/30".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert_eq!(
             ips,
@@ -229,88 +229,88 @@ mod tests {
         );
     }
 
-    #[test]
-    fn parse_correct_host_addresses() {
+    #[tokio::test]
+    async fn parse_correct_host_addresses() {
         let opts = Opts {
             addresses: vec!["google.com".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
-        assert_eq!(ips.len(), 1);
+        assert!(!ips.is_empty());
     }
 
-    #[test]
-    fn parse_correct_and_incorrect_addresses() {
+    #[tokio::test]
+    async fn parse_correct_and_incorrect_addresses() {
         let opts = Opts {
             addresses: vec!["127.0.0.1".to_owned(), "im_wrong".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert_eq!(ips, [Ipv4Addr::new(127, 0, 0, 1),]);
     }
 
-    #[test]
-    fn parse_incorrect_addresses() {
+    #[tokio::test]
+    async fn parse_incorrect_addresses() {
         let opts = Opts {
             addresses: vec!["im_wrong".to_owned(), "300.10.1.1".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert!(ips.is_empty());
     }
-    #[test]
-    fn parse_hosts_file_and_incorrect_hosts() {
+    #[tokio::test]
+    async fn parse_hosts_file_and_incorrect_hosts() {
         // Host file contains IP, Hosts, incorrect IPs, incorrect hosts
         let opts = Opts {
             addresses: vec!["fixtures/hosts.txt".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
-        assert_eq!(ips.len(), 3);
+        assert!(ips.len() >= 3);
     }
 
-    #[test]
-    fn parse_empty_hosts_file() {
+    #[tokio::test]
+    async fn parse_empty_hosts_file() {
         // Host file contains IP, Hosts, incorrect IPs, incorrect hosts
         let opts = Opts {
             addresses: vec!["fixtures/empty_hosts.txt".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert_eq!(ips.len(), 0);
     }
 
-    #[test]
-    fn parse_naughty_host_file() {
+    #[tokio::test]
+    async fn parse_naughty_host_file() {
         // Host file contains IP, Hosts, incorrect IPs, incorrect hosts
         let opts = Opts {
             addresses: vec!["fixtures/naughty_string.txt".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert_eq!(ips.len(), 0);
     }
 
-    #[test]
-    fn parse_duplicate_cidrs() {
+    #[tokio::test]
+    async fn parse_duplicate_cidrs() {
         let opts = Opts {
             addresses: vec!["79.98.104.0/21".to_owned(), "79.98.104.0/24".to_owned()],
             ..Default::default()
         };
 
-        let ips = parse_addresses(&opts);
+        let ips = parse_addresses(&opts).await;
 
         assert_eq!(ips.len(), 2_048);
     }
